a00147e625b624c3432c6caea9bf680ab9d97a83
Chepurnykh Denis (DC6) <denisstarzcontact@gmail.com>
2024-06-08 09:05:02 +0300

    Add additional files of 'platonicSolid'.

---
+++ 20240607/platonicSolid/lib.js
@@ -0,0 +1,12 @@
+/*
+ * FILE NAME   : lib.js
+ * PROGRAMMER  : DC6
+ * LAST UPDATE : 07.06.2024
+ * PURPOSE     : Java script library for all modules file.
+ */
+
+import "./mathLib/camera.js";
+import "./mathLib/vec3.js";
+import "./mathLib/mat4.js";
+
+/* END OF 'lib.js' FILE */
---
+++ 20240607/platonicSolid/render.js
@@ -0,0 +1,8 @@
+/*
+ * FILE NAME   : render.js
+ * PROGRAMMER  : DC6
+ * LAST UPDATE : 07.06.2024
+ * PURPOSE     : Main render module java script file.
+ */
+
+/* END OF 'render.js' FILE */
