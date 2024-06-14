7175d84bd085f9e80a18218f0b13b3f96e9b9ca5
Chepurnykh Denis (DC6) <denisstarzcontact@gmail.com>
2024-06-14 17:37:56 +0300

    Removed 'oldPlatonicSolid'.

--- 20240607/oldPlatonicSolid/resLib/buffers.js
+++
@@ -1,8 +0,0 @@
-/*
- * FILE NAME   : buffers.js
- * PROGRAMMER  : DC6
- * LAST UPDATE : 07.06.2024
- * PURPOSE     : OpenGL buffers (VB, IB, UBO, etc) java script library file.
- */
-
-/* END OF 'buffers.js' FILE */
