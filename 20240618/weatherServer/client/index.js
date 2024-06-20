1c44cfeefac959bbda8083743df0f0b966cc4642
Chepurnykh Denis (DC6) <denisstarzcontact@gmail.com>
2024-06-20 18:16:28 +0300

    Progress in 'weatherServer' project.

--- 20240618/weatherServer/client/index.js
+++ 20240618/weatherServer/client/index.js
@@ -1,60 +1,150 @@
 /*
  * FILE NAME   : index.js
  * PROGRAMMER  : DC6
- * LAST UPDATE : 19.06.2024
+ * LAST UPDATE : 20.06.2024
  * PURPOSE     : Weather index javascript file.
  */
 
-// Lat and Lon of SPb
-// const lat = 59.94324825369481;
-// const lon = 30.278047516212283;
-// var ppm = require("ppm");
-
 import {PixelFontCanvas} from "./lib/PixelFontCanvas.js";
 
-const apiKEY = `4a39ca120c04f12899bae89cb5081145`;
+let socket = new WebSocket("ws://localhost:8000");
 
-const getWetherJSON = url => {
-  fetch(url)
-       .then(response => response.json())
-       .then(json => {
-         let img = new Image();
-         let canv = document.createElement("canvas");
-         let ctx = canv.getContext("2d");
-         canv.width = 32;
-         canv.height = 16;
-         ctx.fillStyle = "#000000";
-         ctx.imageSmoothingEnabled = false;
-         ctx.fillRect(0, 0, canv.width, canv.height);
-         img.src = `../png/${json.weather[0].icon}.png`;
-         img.onload = () => {
-           ctx.drawImage(img, 2, 2);
-           ctx.fillStyle = "#FFFFFF";
-           ctx.font = "6px helvetica";
-           ctx.textRendering = "geometricPrecision";
-           ctx.fillText(`${json.main.temp.toFixed(0)}°C`, 17, 11);
-           document.body.insertAdjacentElement("beforeend", canv);
-           let imgDataRGBA = ctx.getImageData(0, 0, canv.width, canv.height).data;
-           let imgDataRGB = new Uint8ClampedArray(canv.width * canv.height * 3);
-           let pos = 0;
-           for (let i = 0; i < canv.width * canv.height * 4; i += 4) {
-             imgDataRGB[pos] = imgDataRGBA[i];
-             imgDataRGB[pos++] = imgDataRGBA[i + 1];
-             imgDataRGB[pos++] = imgDataRGBA[i + 2];
-           }
-           console.log(imgDataRGBA);
-           console.log(imgDataRGB);
-           console.log(json);
-         }
-       });
+PixelFontCanvas.loadFont("fonts/", "cg.fnt");
+
+let loop;
+let apiKEY;
+fetch("../key.env").then(response => response.text()).then(text => {
+  apiKEY = text;  
+});
+
+let lat;
+let lon;
+
+function restart() {
+  const canv = document.getElementById("can");
+  const text = document.getElementById("data");
+  const endTrack = document.getElementById("endTrack");
+  const but = document.createElement("input");
+  const div = document.getElementById("div");
+  text.disabled = false;
+  endTrack.remove();
+  but.type = "button";
+  but.value = "Start";
+  but.setAttribute("id", "track");
+  but.onclick = function() {trackWeather()};
+  div.appendChild(but);
+  text.value = "";
+  clearInterval(loop);
+  if (canv != undefined) {
+    canv.remove();
   }
+} // End of 'restart' function
 
-function getWeather() {
-  const lat = document.getElementById("lat");
-  const lon = document.getElementById("lon");
-  getWetherJSON(`https://api.openweathermap.org/data/2.5/weather?lat=${lat.value}&lon=${lon.value}&appid=${apiKEY}&units=metric&lang=en`);
-} // End of 'getWeather' function
+function generateCanvas(data) {
+  let canv = document.getElementById("can");
+  if (canv != undefined) {
+    canv.remove();
+  }
+  let img = new Image();
+  canv = document.createElement("canvas");
+  let ctx = canv.getContext("2d");
+  canv.width = 32;
+  canv.height = 16;
+  canv.setAttribute("id", "can");
+  ctx.fillStyle = "#000000";
+  ctx.imageSmoothingEnabled = false;
+  ctx.fillRect(0, 0, canv.width, canv.height);
+  img.src = `../png/${data[1]}.png`;
+  img.onload = () => {
+    ctx.drawImage(img, 2, 2);
+    PixelFontCanvas.drawText(canv, `${data[0]}'`, {
+      font: 'CG',
+      x: data[0] < 0 ? 17 : 19,
+      y: 6,
+      scale: 1,
+      align: 'left',
+      tint: '#FFFFFF'
+    });
+    document.body.insertAdjacentElement("afterend", canv);
+    let imgDataRGBA = ctx.getImageData(0, 0, canv.width, canv.height).data;
+    let imgDataRGB = new Uint8ClampedArray(canv.width * canv.height * 3);
+    let pos = 0;
+    for (let i = 0; i < canv.width * canv.height * 4; i += 4) {
+      imgDataRGB[pos] = imgDataRGBA[i];
+      imgDataRGB[pos++] = imgDataRGBA[i + 1];
+      imgDataRGB[pos++] = imgDataRGBA[i + 2];
+    }
+    socket.send(imgDataRGB);
+  }
+} // End of 'generateCanvas' function
 
-document.getElementById("sendData").onclick = function() {getWeather()};
+function trackWeather() {
+  let oldData;
+  let data;
+  const text = document.getElementById("data"); 
+  const array = text.value.split(", ");
+  const track = document.getElementById("track");
+  const div = document.getElementById("div");
+  track.remove();
+  const endTrack = document.createElement("button");
+  endTrack.textContent = "Stop";
+  endTrack.setAttribute("id", "endTrack");
+  endTrack.onclick = () => {
+    restart();
+  }
+  div.appendChild(endTrack);
+  text.disabled = true;
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${array[0]},${array[1]}&appid=${apiKEY}&units=metric&lang=en`)
+    .then(response => response.json())
+    .then(json => {
+      if (json.length == 0) {
+        alert(`Failed to get weather data`);
+        restart();
+        return;
+      }
+      lat = json[0].lat;
+      lon = json[0].lon;
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${json[0].lat}&lon=${json[0].lon}&appid=${apiKEY}&units=metric&lang=en`)
+        .then(response => response.json())
+        .then(json => {
+          if (json.length == 0) {
+            alert(`Failed to get weather data`);
+            restart();
+            return;
+          }
+          data = [json.main.temp.toFixed(0), json.weather[0].icon];
+          generateCanvas(data);
+          oldData = data;
+          loop = setInterval(() => {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKEY}&units=metric&lang=en`)
+              .then(response => response.json())
+              .then(json => {
+                if (json.length == 0) {
+                  alert(`Failed to get weather data`);
+                  restart();
+                  return;
+                }
+                data = [json.main.temp.toFixed(0), json.weather[0].icon];
+                if (oldData[0] != data[0] || oldData[1] != data[1]) {
+                  oldData = data;
+                  generateCanvas(data);
+                } 
+            });
+          }, 60000);
+        });
+      });
+} // End of 'trackWeather' function
+
+function getPPM() {
+  socket.onmessage = (event) => {
+    let ppm = event.data;
+    ppm = JSON.parse(ppm);
+    console.log(ppm);
+  }
+} // End of 'getPPM' function
+
+document.getElementById("track").onclick = function() {trackWeather()};
+
+getPPM();
 
 /* END OF 'index.js' FILE */
--- 20240618/weatherServer/main.js
+++ 20240618/weatherServer/main.js
@@ -1,55 +1,42 @@
 /*
  * FILE NAME   : main.js
  * PROGRAMMER  : DC6
- * LAST UPDATE : 18.06.2024
+ * LAST UPDATE : 20.06.2024
  * PURPOSE     : Weather server server java script file.
  */
 
-/*
 import http from "node:http";
 import {WebSocketServer} from "ws";
 import express from "express";
+import favicon from "serve-favicon";
 
 const app = express();
 app.get('/', (req, res, next) => {
   next();
 });
+app.use(favicon('favicon.ico'));
 app.use(express.static("client"));
 
 const server = http.createServer(app);
 
 const wss = new WebSocketServer({server});
 
-function addClient(ws, wss, WebSocket) {
-  ws.userid = max_connected;
-  max_connected += 1;
-  clients.push(ws)
-
-  console.log("Client Connect: " + ws.userid + ". " + clients.length + " Online")
-
-  ws.onmessage = (msg) => {
-    let m = msg.data;
-
-    for (let cl of clients) {
-      cl.send(m);
-    }
-  }
-
-  ws.on('close', () =>  {
-    removeClient(ws);
-  });
-}
-
 wss.on("connection", (ws) => {
-  addClient(ws, wss);
+
+  ws.onmessage = (data) => {
+    let image = data.data;
+    let ppm = image;
+    ppm = JSON.stringify(ppm);
+
+    ws.send(ppm);
+  }
 })
 
-const host = `localhost`;
+const host = `127.0.0.1`;
 const port = 8000;
 
 server.listen(port, host, () => {
   console.log(`Server started on http://${host}:${port}`);
 })
-*/
 
 /* END OF 'main.js' FILE */
